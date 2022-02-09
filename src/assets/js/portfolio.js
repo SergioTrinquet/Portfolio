140927bb195c20fe4c2eedbcd4a6563806b9b1e4
SergioTrinquet <serge.trinquet@gmail.com>
2022-02-09 23:41:07 +0100

    Test pour corriger svg qui disparait qd anim avec iOS (test suite)

--- src/assets/js/portfolio.js
+++ src/assets/js/portfolio.js
@@ -426,7 +426,7 @@ function generate_timeline() {
         ] }, "<+=25")
         .to("#SVG_tasse", { keyframes: [
             { display: "initial", autoAlpha: 1, duration: 20 ,transform:"translate3d(0, -200vh, 0)" },
-            { y:0, duration: 25 }
+            { /* y:0, */transform:"translate3d(0, 0, 0)", duration: 25 }
         ] }, "<+=25")
         .to("#SVG_ombre", { display: "initial", autoAlpha:1, duration: 25 });
 
