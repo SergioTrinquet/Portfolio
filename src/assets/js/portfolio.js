90a966fe08bec7a4e5a1e6a4603c5c489c90e58d
SergioTrinquet <serge.trinquet@gmail.com>
2022-02-09 16:26:31 +0100

    Annulation dernier test

--- src/assets/js/portfolio.js
+++ src/assets/js/portfolio.js
@@ -38,39 +38,6 @@ document.querySelector("#titi").innerText = isIPadOrIPhone ? "iPhone/iPad" : "Au
 
 let scrubValue = 1;
 
-
-
-var browser = window,
-      doc = browser.document;
-
-  // If there's a hash, or addEventListener is undefined, stop here
-  if ( !location.hash || !browser.addEventListener ) {
-
-    //set to 1
-    window.scrollTo( 0, 1 );
-    var scrollTop = 1,
-
-    //reset to 0 if needed
-    checkWindowBody = setInterval(function(){
-      if( doc.body ){
-        clearInterval( checkWindowBody );
-        scrollTop = "scrollTop" in doc.body ? doc.body.scrollTop : 1;
-        browser.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
-      } 
-    }, 15 );
-
-    if (browser.addEventListener) {
-      browser.addEventListener("load", function(){
-        setTimeout(function(){
-          //reset to hide address
-          browser.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
-        }, 0);
-      }, false );
-    }
-  }
-
-
-
 // Code juste pour mobile : Correct° du bug sur mobile et tablettes => unité du type vh, vmax, vmin,... sont faussées à cause de la barre d'adresse qui coulisse
 if (isMobileOrTablette) {
     function setCSSunits() {
