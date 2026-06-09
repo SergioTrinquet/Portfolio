0b46a93ddf54bc8b3c9b7f0c48bf899b8787fc56
SergioTrinquet <serge.trinquet@gmail.com>
2026-06-09 11:33:06 +0200

    fix: update project card animation logic and improve handling to be able to stop correctly on projects

--- src/assets/js/portfolio.js
+++ src/assets/js/portfolio.js
@@ -320,16 +320,18 @@
 
         // Suppression du CSS dans les balises styles ajouté par GSAP
         tl_scrollTriggerBody
-            .set(`.wrapper-SVGs-and-texts, .rayons, .text-presentation, #bg-screen-1-and-2 > .ray, 
-            #content-screen-3, .pre-screen-3, #content-screen-3 #shadows > div, .halo, .half-screen-bg, #skills .domain, .domain .title, #SVGs, #intitule-job, #content-screen-4, #section-titles .section-title, #skills, 
-            #bg-screen-5, #SVG-chaise, #SVG-table-sans-pied-BG, #SVG-table-pied-BG, #SVG-corps, #SVG-bras,#SVG-laptop, #SVG-lampe, #SVG-tasse, #SVG-ombre, .msg-remerciements, .msg-remerciements > *, #marge-right,
-            #bg-screen-end, #bg-screen-end #mot span, #bg-screen-end .mot-trait, .bg-transitional, .SVGs-and-annexes
-            `, {clearProps: "all"});
             /* .set(`.wrapper-SVGs-and-texts, .rayons, .text-presentation, #bg-screen-1-and-2 > .ray, 
             #content-screen-3, .pre-screen-3, #content-screen-3 #shadows > div, .halo, .half-screen-bg, #skills .domain, .domain .title, #SVGs, #intitule-job, #content-screen-4, #section-titles .section-title, #skills, 
             #bg-screen-5, #SVG-chaise, #SVG-table-sans-pied-BG, #SVG-table-pied-BG, #SVG-corps, #SVG-bras,#SVG-laptop, #SVG-lampe, #SVG-tasse, #SVG-ombre, .msg-remerciements, .msg-remerciements > *, #marge-right,
             #bg-screen-end, #bg-screen-end #mot span, #bg-screen-end .mot-trait, .bg-transitional, .SVGs-and-annexes, .project-card
             `, {clearProps: "all"}); */
+            .set(`.wrapper-SVGs-and-texts, .rayons, .text-presentation, #bg-screen-1-and-2 > .ray, 
+            #content-screen-3, .pre-screen-3, #content-screen-3 #shadows > div, .halo, .half-screen-bg, #skills .domain, .domain .title, #SVGs, #intitule-job, #content-screen-4, #section-titles .section-title, #skills, 
+            #bg-screen-5, #SVG-chaise, #SVG-table-sans-pied-BG, #SVG-table-pied-BG, #SVG-corps, #SVG-bras,#SVG-laptop, #SVG-lampe, #SVG-tasse, #SVG-ombre, .msg-remerciements, .msg-remerciements > *, #marge-right,
+            #bg-screen-end, #bg-screen-end #mot span, #bg-screen-end .mot-trait, .bg-transitional, .SVGs-and-annexes
+            `, {clearProps: "all"});
+                
+        if(isIPadOrIPhone) tl_scrollTriggerBody.set(".SVGs-and-annexes", { width: "70vmin" }); // Pour iOS, contairement à Android et nav. PC, on doit donner une largeur a cet élément pour qu'il y ait animation, sinon change de dimension "par à-coups"
                 
         if(isIPadOrIPhone) tl_scrollTriggerBody.set(".SVGs-and-annexes", { width: "70vmin" }); // Pour iOS, contairement à Android et nav. PC, on doit donner une largeur a cet élément pour qu'il y ait animation, sinon change de dimension "par à-coups"
 
@@ -439,7 +441,7 @@
                 { y: "100vh", autoAlpha: 1, scale: 1 }, 
                 { y: "0vh", autoAlpha: 1, scale: 1, duration: 40, stagger: 10 }
             ); // Arrivée encarts projets venant du bas
-            
+
         // Ajout projets
         setProjectCards(intitulesMenu[3]);
 
@@ -608,23 +610,26 @@
     function setProjectCards(intituleMenu) {
         const cards = document.querySelectorAll("#projects .project-card");
         let units = 100;
-        for(var i = 1; i <= nbProjectCards; i++) {
+
+        // Etat initial (Projet 1 déjà centré)
+        tl_scrollTriggerBody
+            .to(".project-card", { duration: 50 })
+            .addLabel(`${prefixNomLabelProjets}_1|${intituleMenu}`, ">")
+            .to(".project-card", { duration: 50 });
+
+        for(var i = 1; i < nbProjectCards; i++) {
             let coordY = -(i * units);
-            // On ne cible que les cards qui ne sont pas encore "fixées" (stackées)
-            // La card 1 reste à 0vh, la card 2 à -100vh, etc.
             const targets = Array.from(cards).slice(i);
             
             tl_scrollTriggerBody
-                .to(".project-card", { duration: 25 })
-                .addLabel(`${prefixNomLabelProjets}_${i}|${intituleMenu}`, ">") 
-                .to(targets, { y: `${coordY}vh`, duration: 80, stagger: 10 });
-
-            // Effet de fading et scale sur la carte qui va être recouverte
-            if (i < nbProjectCards) {
-                tl_scrollTriggerBody.to(cards[i-1], { autoAlpha: 0, scale: 0.8, duration: 40 }, "<");
-            }
-
-            tl_scrollTriggerBody.to(".project-card", { duration: 25 }) // Pour que l'on reste sur un projet qd on scroll, sinon effet d'inertie et passe au label suivant
+                // Transition vers le projet suivant
+                .to(targets, { y: `${coordY}vh`, duration: 80, stagger: 10 })
+                .to(cards[i-1], { autoAlpha: 0, scale: 0.8, duration: 40 }, "<")
+                
+                // Etat stable (Le projet i+1 est maintenant en place)
+                .to(".project-card", { duration: 50 })
+                .addLabel(`${prefixNomLabelProjets}_${i+1}|${intituleMenu}`, ">") 
+                .to(".project-card", { duration: 50 });
         }
         return tl_scrollTriggerBody;
     }
