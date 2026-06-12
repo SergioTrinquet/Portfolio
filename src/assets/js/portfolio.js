fcf6f5c4c3065c94df8abb7c81fddeaef1f3f184
SergioTrinquet <serge.trinquet@gmail.com>
2026-06-12 10:43:58 +0200

    fix: remove unnecessary whitespace in animation settings for iOS compatibility

--- src/assets/js/portfolio.js
+++ src/assets/js/portfolio.js
@@ -332,9 +332,7 @@
             #bg-screen-5, #SVG-chaise, #SVG-table-sans-pied-BG, #SVG-table-pied-BG, #SVG-corps, #SVG-bras,#SVG-laptop, #SVG-lampe, #SVG-tasse, #SVG-ombre, .msg-remerciements, .msg-remerciements > *, #marge-right,
             #bg-screen-end, #bg-screen-end #mot span, #bg-screen-end .mot-trait, .bg-transitional, .SVGs-and-annexes
             `, {clearProps: "all", /* "--y-coord": 0 */"--y-coord": 100, "--top-half-screen-bg": 45}); // Ajout propriétés CSS personnalisées utilisées pour les animations
-                
-        if(isIPadOrIPhone) tl_scrollTriggerBody.set(".SVGs-and-annexes", { width: "70vmin" }); // Pour iOS, contairement à Android et nav. PC, on doit donner une largeur a cet élément pour qu'il y ait animation, sinon change de dimension "par à-coups"
-                
+                      
         if(isIPadOrIPhone) tl_scrollTriggerBody.set(".SVGs-and-annexes", { width: "70vmin" }); // Pour iOS, contairement à Android et nav. PC, on doit donner une largeur a cet élément pour qu'il y ait animation, sinon change de dimension "par à-coups"
 
         tl_scrollTriggerBody
